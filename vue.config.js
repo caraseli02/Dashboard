3c0e06c64d130342f9c560f0d0bc70ad5f0cd40c
vlad <caraseli02@gmail.com>
2021-04-12 08:56:18 +0200

    add pwa assets generator

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,10 @@
 module.exports = {
   // ...other vue-cli plugin options...
   pwa: {
-    manifestOptions: {
-      name: process.env.VUE_APP_APP_NAME,
-      short_name: process.env.VUE_APP_SHORT_NAME,
-      start_url: process.env.VUE_APP_START_URL,
-      display: "standalone",
-      theme_color: process.env.VUE_APP_PRIMARY_COLOR,
-      background_color: process.env.VUE_APP_BACKGROUND_COLOR,
-      iconPaths: {
-        favicon32: `"img/icons/${process.env.VUE_APP_LOGO}-32x32.png"`,
-        favicon16: `"img/icons/${process.env.VUE_APP_LOGO}-16x16.png"`,
-        appleTouchIcon: `"img/icons/${process.env.VUE_APP_LOGO}-152x152.png"`,
-        maskIcon: `"img/icons/${process.env.VUE_APP_LOGO}-safari.svg"`,
-        msTileImage: `"img/icons/${process.env.VUE_APP_LOGO}-144x144.png"`,
-      },
-      icons: [
-        {
-          src: `"img/icons/${process.env.VUE_APP_LOGO}-192x192.png"`,
-          sizes: "192x192",
-          type: "image/png",
-        },
-        {
-          src: `"img/icons/${process.env.VUE_APP_LOGO}-512x512.png"`,
-          sizes: "512x512",
-          type: "image/png",
-        },
-      ],
-    },
+    name: process.env.VUE_APP_APP_NAME,
+    themeColor: process.env.VUE_APP_BACKGROUND_COLOR,
+    msTileColor: process.env.VUE_APP_PRIMARY_COLOR,
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
   },
-  // pwa: {
-  //   name: "Caraseli App",
-  //   themeColor: "#FFFFFF",
-  //   msTileColor: "#0C33F5",
-  //   appleMobileWebAppCapable: "yes",
-  //   appleMobileWebAppStatusBarStyle: "black",
-  // },
 };
